34d8f12b6aa0562c57c94c52ed6e6555812cc8cc
bright0220 <happysuperstar601@gmail.com>
2024-07-02 14:04:27 -0400

    fix: updated landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,52 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
-import Card from "../../components/Card";
-import Hero from "../../components/Hero";
-import { useRouter } from "next/navigation";
-import { supabase } from "@/supabase";
 
 const Landing = () => {
-  const [posts, setPosts] = useState<any>([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-  const fetchPosts = async () => {
-    const { data, error } = await supabase
-      .from("posts")
-      .select("*")
-      .order("created_at", { ascending: false }) // Order by descending to get the latest rows first
-      .limit(8);
-    if (data) {
-      setPosts(data);
-    } else {
-      console.log(error);
-    }
-  };
   return (
-    <main className="flex h-full w-full flex-col items-center justify-between">
-      <div className="pt-10 pb-28">
-        <Hero />
-      </div>
-      <div className="w-full pt-10 pb-36 flex flex-row justify-center top-0 sticky bg-slate-200">
-        <div className="">
-          <div className="text-3xl font-bold">Latest Post</div>
-          <div className="pt-8 max-w-6xl grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
-            {posts.map((post: any, index: number) => (
-              <div key={index}>
-                <Card post={post} />
-              </div>
-            ))}
-          </div>
-          <div className="pt-8 flex flex-row justify-center">
-            <button className="px-4 py-2 bg-slate-400 rounded-xl">
-              Load More
-            </button>
-          </div>
-        </div>
-      </div>
-    </main>
+    <main className="flex h-full w-full flex-col items-center justify-between"></main>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
                 />
                 <Header />
                 {children}
-                <Footer />
+                {/* <Footer /> */}
               </ToastProvider>
             </AuthProvider>
           </UserProvider>
