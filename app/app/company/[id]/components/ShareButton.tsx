14f49adb2e2ed6af27e0ed7972764d9c57bdeae8
bright0220 <happysuperstar601@gmail.com>
2024-07-17 07:25:32 -0400

    refactored

--- app/app/company/[id]/components/ShareButton.tsx
+++ app/app/company/[id]/components/ShareButton.tsx
@@ -51,7 +51,7 @@ const ShareButton = () => {
     <>
       <button
         onClick={handleShare}
-        className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-full shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex items-center transition duration-150 ease-in-out"
+        className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-full shadow-md hover:bg-gray-100 focus:outline-none flex items-center transition duration-150 ease-in-out"
       >
         <ShareSvg />
         Share
