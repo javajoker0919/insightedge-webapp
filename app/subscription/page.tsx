a1237bc9ce0969a582dec430e776f162671a69f7
bright0220 <happysuperstar601@gmail.com>
2024-08-01 16:25:33 -0400

    fix: refactored

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -164,9 +164,7 @@ const PlanHeader: React.FC<PlanHeaderProps> = ({
         <p className="text-sm text-gray-500">/ user / month</p>
       </div>
       <button
-        className={`w-full py-2 px-4 border border-gray-300 rounded-md text-white ${
-          title == currentPlan?.toUpperCase() ? "bg-gray-500" : "bg-primary-500"
-        }`}
+        className={`w-full py-2 px-4 border border-gray-300 bg-primary-500 disabled:bg-gray-400 rounded-md text-white disabled:cursor-not-allowed`}
         onClick={() => handleSubscribe(title)}
         disabled={isLoading || currentPlan?.toUpperCase() == title}
       >
