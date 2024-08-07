1e4a41704b9f6649fdbed027ccfd683789237771
altl-21 <blackphantom0221@gmail.com>
2024-08-07 17:07:25 -0400

    feat: implement new onboarding flow

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -27,29 +27,27 @@ const SignUp = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    confirmPass: "",
+    confirmPass: ""
   });
 
   const [errors, setErrors] = useState({
     email: "",
     password: "",
-    confirmPass: "",
+    confirmPass: ""
   });
 
   /// State to track form validation, checkbox, and loading
   const [isValidate, setIsValidate] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   /// Effect to check form validity
   useEffect(() => {
     const { email, password, confirmPass } = formData;
     const hasErrors = Object.values(errors).some((error) => error !== "");
-    const allFieldsFilled = Boolean(email && password && confirmPass);
-    const passwordsMatch = password === confirmPass;
+    const allFieldsFilled = Boolean(email && password);
 
-    setIsValidate(allFieldsFilled && !hasErrors && passwordsMatch && isChecked);
-  }, [formData, errors, isChecked]);
+    setIsValidate(allFieldsFilled && !hasErrors);
+  }, [formData, errors]);
 
   /// Handle input changes and validate fields
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,11 +63,6 @@ const SignUp = () => {
     }
   };
 
-  /// Handle checkbox change for terms agreement
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(e.target.checked);
-  };
-
   /// Handle form submission for user registration
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -83,8 +76,8 @@ const SignUp = () => {
         email,
         password,
         options: {
-          emailRedirectTo: "http://localhost:3000/",
-        },
+          emailRedirectTo: "http://localhost:3000/onboarding/user-info"
+        }
       });
 
       if (authError) throw authError;
@@ -108,11 +101,11 @@ const SignUp = () => {
         email: userData.email,
         firstName: userData.first_name || "",
         lastName: userData.last_name || "",
-        onboardingStatus: false,
+        onboardingStatus: false
       });
 
-      invokeToast("success", "Successfully signed up!", "top");
-      router.replace("/auth/create-profile");
+      // invokeToast("success", "Successfully signed up!", "top");
+      router.replace("/auth/verify-email");
     } catch (error: any) {
       console.error("Sign-up error:", error);
       invokeToast("error", error.message || "Something went wrong!", "top");
@@ -123,105 +116,153 @@ const SignUp = () => {
 
   /// Render the sign-up form
   return (
-    <div className="bg-white w-full h-screen">
-      <div className="flex flex-col lg:flex-row justify-center h-full">
-        <div className="h-full px-5 flex items-center justify-center">
-          <div className="flex flex-col gap-8 mb-16">
-            <div className="md:w-[400px] w-full md:m-auto shadow-primary-200 rounded-xl md:shadow-2xl md:p-8">
-              <header className="text-center mb-8">
-                <h2 className="text-gray-900 text-2xl font-semibold mb-2">
-                  Create your account
-                </h2>
-              </header>
-
-              <form onSubmit={handleFormSubmit} className="flex flex-col gap-5">
-                <div className="space-y-1">
-                  <p className="text-slate-600">Email</p>
-                  <AuthInput
-                    name="email"
-                    type="email"
-                    error={errors.email}
-                    onChange={handleInputChange}
-                  />
-                </div>
-
-                <div className="space-y-1">
-                  <p className="text-slate-600">Password</p>
-                  <AuthInput
-                    name="password"
-                    type="password"
-                    error={errors.password}
-                    onChange={handleInputChange}
-                  />
-                </div>
-
-                <div className="space-y-1">
-                  <p className="text-slate-600">Confirm Password</p>
-                  <AuthInput
-                    name="confirmPass"
-                    type="password"
-                    error={errors.confirmPass}
-                    onChange={handleInputChange}
-                  />
-                </div>
-
-                <div className="flex items-center space-x-3">
-                  <input
-                    className="w-5 h-5 focus:ring-transparent rounded-full border border-bgray-300 focus:accent-success-300 text-success-300"
-                    id="remember"
-                    type="checkbox"
-                    name="remember"
-                    checked={isChecked}
-                    onChange={handleCheckboxChange}
-                  />
-                  <label
-                    htmlFor="remember"
-                    className="text-slate-600 text-base flex flex-col items-center"
-                  >
-                    <p>By creating an account, you agree to our</p>
-                    <span className="text-primary-500 font-semibold">
-                      Terms of Use
-                    </span>
-                  </label>
-                </div>
-
-                <button
-                  type="submit"
-                  disabled={!isValidate || isLoading}
-                  className="py-3.5 mt-2 flex items-center justify-center text-white bg-primary-600 hover:bg-primary-700 transition-all rounded-lg w-full disabled:bg-gray-300 disabled:cursor-not-allowed"
-                >
-                  {isLoading ? (
-                    <span className="inline-block animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-white"></span>
-                  ) : (
-                    "Sign Up"
-                  )}
-                </button>
-              </form>
-
-              <div className="flex flex-col gap-4 mt-4">
-                <div className="inline-flex w-full cursor-pointer justify-center items-center gap-x-2 text-black border border-gray-300 rounded-lg px-4 py-3 text-base font-medium hover:bg-slate-200">
-                  <Image
-                    src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg"
-                    alt="google_logo"
-                    width={20}
-                    height={20}
-                  />
-                  <span className="text-base font-semibold ">
-                    Sign Up with Google
-                  </span>
-                </div>
+    <div className="flex flex-row w-full h-screen">
+      <div className="flex flex-col w-1/2 h-full bg-white">
+        <div className="flex items-center  mt-6 ml-6">
+          <Image
+            src="/favicon.png"
+            alt="ProspectEdge Logo"
+            width={40}
+            height={40}
+          />
+          <Image src="/logo.png" alt="ProspectEdge" width={200} height={40} />
+        </div>
+        <div className="flex flex-col mt-24 items-center w-full">
+          <div className="flex flex-col w-[26rem] text-center">
+            <h1 className="text-3xl font-bold text-gray-900 leading-[48px] mb-3">
+              Create an account
+            </h1>
+            <div className="flex flex-col items-center justify-center max-w-[342px] w-full mx-auto text-center text-gray-600 mb-4 text-sm font-normal leading-[22px]">
+              <p>By creating an account, you agree to this app's</p>
+              <div className="flex flex-row gap-1 text-sm">
+                <Link href="/" className="text-primary-400 underline font-bold">
+                  Terms
+                </Link>
+                <span>and</span>
+                <Link href="/" className="text-primary-400 underline font-bold">
+                  Privacy Policy
+                </Link>
               </div>
             </div>
-
-            <div className="text-center flex items-center gap-2 justify-center text-gray-900 text-base font-medium">
-              <Link
-                href="/auth/sign-in"
-                className="font-semibold text-primary-400"
-              >
-                Sign In
-              </Link>
-              <p className="text-slate-600">if you already have an account</p>
+            <button className="w-full flex justify-center items-center gap-2 cursor-pointer text-base py-4 font-normal leading-6 rounded-full text-black border border-gray-300 hover:bg-gray-100 transition-colors duration-200">
+              <Image
+                src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg"
+                alt="Google logo"
+                width={20}
+                height={20}
+              />
+              <span className="text-base font-semibold">
+                Sign Up with Google
+              </span>
+            </button>
+            <div className="flex items-center w-full mt-8 mb-6">
+              <div className="flex-grow border-t border-gray-300"></div>
+              <span className="flex-shrink mx-4 text-sm font-bold text-gray-500">
+                OR SIGN UP WITH YOUR EMAIL
+              </span>
+              <div className="flex-grow border-t border-gray-300"></div>
             </div>
+            <form
+              // onSubmit={handleFormSubmit}
+              className="flex flex-col w-full mt-3"
+            >
+              <div className="flex flex-col w-full">
+                <label
+                  htmlFor="email"
+                  className="text-left text-base font-bold leading-6 mb-2"
+                >
+                  Email address
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="outline-none text-base font-normal bg-gray-100 leading-6 p-4 rounded-md"
+                  placeholder="example.email@gmail.com"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="flex flex-col w-ufll mt-2">
+                <label
+                  htmlFor="password"
+                  className="text-left text-base font-bold leading-6 mb-2"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="outline-none text-base font-normal bg-gray-100 leading-6 p-4 rounded-md"
+                  placeholder="••••••••"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <button
+                // type="submit"
+                className="w-full bg-primary-600 text-white text-base font-semibold py-4 mt-4 rounded-md hover:bg-primary-700 active:bg-primary-800 transition-colors duration-200 disabled:bg-gray-300 disabled:cursor-not-allowed"
+                disabled={!isValidate || isLoading}
+              >
+                {isLoading ? (
+                  <span className="inline-flex items-center">
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    Signing Up...
+                  </span>
+                ) : (
+                  "Sign Up"
+                )}
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="absolute bottom-8 left-1/4 transform -translate-x-1/2 font-bold text-sm text-gray-600">
+          <span>Have an account? </span>
+          <Link
+            href="/auth/sign-in"
+            className="text-primary-400 hover:underline"
+          >
+            Log in
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center justify-center bg-gray-50 w-1/2 h-full">
+        <div className="flex flex-col items-center">
+          <div className="text-center mb-[70px]">
+            <p className="text-[32px] leading-[48px] font-bold text-gray-700">
+              Welcome to ProspectEdge!
+            </p>
+            <p className="text-lg font-normal leading-7 text-gray-600">
+              First things first, let's set you up with an account! 👋
+            </p>
+          </div>
+          <div className="relative">
+            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 rounded-[50%] bg-gray-200 w-[600px] h-[8.5rem] z-0"></div>
+            <Image
+              src="/welcome.svg"
+              width={513}
+              height={416}
+              alt="Welcome illustration"
+              className="relative z-10"
+            />
           </div>
         </div>
       </div>
