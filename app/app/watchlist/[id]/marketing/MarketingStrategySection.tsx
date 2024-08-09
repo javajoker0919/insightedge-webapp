ae6f0c76bb35ade3f64f52f2ae5761f324600305
bright0220 <happysuperstar601@gmail.com>
2024-08-09 10:15:35 -0400

    fix: updated folder name

--- app/app/watchlist/[id]/marketing/MarketingStrategySection.tsx
+++ app/app/watchlist/[id]/marketing/MarketingStrategySection.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from "react";
-import { marketingStrategy } from "@/app/app/company/[id]/Constants";
-import MarketingStrategyTable from "@/app/app/company/[id]/Marketing/MarketingStrategyTable";
-import MarketingPlanModal from "@/app/app/company/[id]/Marketing/MarketingPlanModal";
 
 interface MarketingCompProps {
   companyID: number;
