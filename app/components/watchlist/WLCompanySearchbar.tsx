b2f1dffa9bed57a6b6a83194cda10be243268a04
bright0220 <happysuperstar601@gmail.com>
2024-08-27 04:23:48 -0400

    feat: updated search algorithm in comany search bar

--- app/components/watchlist/WLCompanySearchbar.tsx
+++ app/components/watchlist/WLCompanySearchbar.tsx
@@ -74,12 +74,12 @@ const WLCompanySearchbar = ({
     if (searchInput.length > 0) {
       if (searchType === "all") {
         query = query.or(
-          `name.ilike.${searchInput}%,symbol.ilike.${searchInput}%`
+          `name.ilike.%${searchInput}%,symbol.ilike.%${searchInput}%`
         );
       } else if (searchType === "name") {
-        query = query.ilike("name", `${searchInput}%`);
+        query = query.ilike("name", `%${searchInput}%`);
       } else if (searchType === "symbol") {
-        query = query.ilike("symbol", `${searchInput}%`);
+        query = query.ilike("symbol", `%${searchInput}%`);
       }
     }
 
