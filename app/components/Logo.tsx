0ea2a018200ec511c33f68fd17ca2f6cd9c046c7
bright0220 <happysuperstar601@gmail.com>
2024-08-30 10:04:18 -0400

    fix: fixed url of clicking logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Logo: React.FC = () => {
   return (
-    <Link href={`/`}>
+    <Link href={`/app`}>
       <Image
         src={"/logo.png"}
         alt={"ProspectEdge"}
