e1646d1ac83ddf5f90462297b9f6e16f666e4c56
bright0220 <happysuperstar601@gmail.com>
2024-09-05 10:44:16 -0400

    fix: removed duplicated codes regarding SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,26 +54,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-537W4DTR');
-          `}
-        </Script>
-      </head>
       <body className="text-black">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-537W4DTR"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         <div className="flex h-full min-h-screen flex-col items-center justify-between bg-white">
           <AuthProvider>
             <ToastProvider>
