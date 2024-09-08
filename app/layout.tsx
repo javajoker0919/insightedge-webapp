930a5cb69f30263faa777ffb344eb882a9691fce
insightedge-create <insightedge1234@gmail.com>
2024-09-08 18:33:37 -0400

    Update layout.tsx
    Updated opengraph title

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   openGraph: {
     url: "https://prospectedge.co/",
     type: "website",
-    title: "ProspectEdge: AI Sales Planning",
+    title: "ProspectEdge: AI Sales Account Planning",
     description:
       "Boost your B2B sales with ProspectEdge's AI-powered insights. Get real-time data on customers, markets, and trends to close more deals.",
     images: [
